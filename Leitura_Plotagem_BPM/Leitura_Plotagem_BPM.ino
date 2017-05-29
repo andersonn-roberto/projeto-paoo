f6d7bcac47dcbd222285f63496c6122b3b83c2fc
Anderson Oliveira <andersonn.roberto@gmail.com>
2017-05-29 20:00:39 -0300

    Botão funcionando

--- Leitura_Plotagem_BPM/Leitura_Plotagem_BPM.ino
+++ Leitura_Plotagem_BPM/Leitura_Plotagem_BPM.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include "pitches.h"
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -17,6 +18,11 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 //  Variables
 int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED13 = 13;   //  The on-board Arduion LED
+int btnLigaDesliga = 9;
+int buzzer = 8;
+int ledVermelho = 7;
+int ledVerde = 6;
+int efetuarLeitura = 0;
 
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
 int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore.
@@ -25,21 +31,64 @@ int Threshold = 550;            // Determine which Signal to "count as a beat", 
 void setup() {
   lcd.begin(16, 2);
   pinMode(LED13, OUTPUT);        // pin that will blink to your heartbeat!
+  pinMode(buzzer, OUTPUT);
+  pinMode(btnLigaDesliga, INPUT_PULLUP);
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
   Serial.begin(9600);         // Set's up Serial Communication at certain speed.
+  digitalWrite(ledVermelho, LOW);
+  digitalWrite(ledVerde, LOW);
 }
 
 // The Main Loop Function
 void loop() {
-  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value.
-  // Assign this value to the "Signal" variable.
 
-  Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
-
-  if (Signal > Threshold) {                        // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
-    digitalWrite(LED13, HIGH);
-  } else {
-    digitalWrite(LED13, LOW);               //  Else, the sigal must be below "550", so "turn-off" this LED.
+  // Check if the btnLigaDesliga was pressed
+  if (digitalRead(btnLigaDesliga) == HIGH) {
+    
+    if (efetuarLeitura == 0) {      
+      
+      efetuarLeitura = 1;
+      Serial.println("Pressionou: Ligar " + (String)efetuarLeitura);
+      delay(500);
+      
+    } else {      
+      
+      efetuarLeitura = 0;      
+      delay(500);
+      
+    }
+    
   }
+  
+  if (efetuarLeitura == 1) {
+     
+    Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value.
+    // Assign this value to the "Signal" variable.
 
-  delay(10);
+    Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
+
+    if (Signal > Threshold) {                        // If the signal is above "550", then "turn-on" Arduino's on-Board LED.
+      digitalWrite(LED13, HIGH);
+      lcd.clear();
+      lcd.setCursor(3, 0);
+      lcd.print((String)Signal + " BPM");
+    } else {
+      digitalWrite(LED13, LOW);               //  Else, the sigal must be below "550", so "turn-off" this LED.
+    }
+
+    tone(buzzer, 262, 100);
+    digitalWrite(ledVermelho, HIGH);
+    digitalWrite(ledVerde, HIGH);
+    
+  } else {
+    
+    noTone(buzzer);
+    digitalWrite(ledVermelho, LOW);
+    digitalWrite(ledVerde, LOW);
+    
+  }
+  
+  delay(2);
+  
 }
